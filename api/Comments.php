5e5e229f3a4cf3815ee3998857c58a851b4940f0
Mathieu Delestre <ldbglobe@gmail.com>
2016-05-24 11:27:53 +0200

    basics method to retrieve comments

--- api/Comments.php
+++ api/Comments.php
@@ -2,5 +2,58 @@
 namespace ldbglobe\qualitelis\api;
 
 class Comments {
+	var $settings = (object)array();
 
+	function __construct($default_settings)
+	{
+		$this->_default_settings($default_settings);
+	}
+
+	private function _default_settings($settings)
+	{
+		$settings = (object)$settings;
+
+		if(isset($settings->Token))
+			$this->settings->Token = $settings->Token;
+		if(isset($settings->IdGroupe))
+			$this->settings->IdGroupe = $settings->IdGroupe;
+		if(isset($settings->IdContractor))
+			$this->settings->IdContractor = $settings->IdContractor;
+		if(isset($settings->Langue))
+			$this->settings->Langue = $settings->Langue;
+		if(isset($settings->VisitorLanguage))
+			$this->settings->VisitorLanguage = $settings->VisitorLanguage;
+	}
+
+	private function _setting($k,$settings,$default)
+	{
+		// extract settings from arguments or fallback to internal default settings
+		$settings = (object)$settings;
+
+		if(isset($settings->{$k}))
+			return $settings->{$k};
+		else if(isset($this->settings->{$k}))
+			return $this->settings->{$k}
+		return null;
+	}
+
+	public function Group($settings)
+	{
+		$Token = $this->_setting('Token',$settings);
+		$IdGroupe = $this->_setting('IdGroupe',$settings);
+		$IdContractor = $this->_setting('IdContractor',$settings);
+		$Langue = $this->_setting('Langue',$settings);
+		$VisitorLanguage = $this->_setting('VisitorLanguage',$settings);
+		$url = "http://www.qualitelis-survey.com/api/Comments/Group?Token={$Token}&IdGroupe={$IdGroupe}&IdContractor={$IdContractor}&Langue={$Langue}&VisitorLanguage={$VisitorLanguage}";
+		return json_decode(file_get_contents($url));
+	}
+	public function AllPrestaGroup($settings)
+	{
+		$Token = $this->_setting('Token',$settings);
+		$IdGroupe = $this->_setting('IdGroupe',$settings);
+		$Langue = $this->_setting('Langue',$settings);
+		$VisitorLanguage = $this->_setting('VisitorLanguage',$settings);
+		$url = "http://www.qualitelis-survey.com/api/Comments/AllPrestaGroup?Token={$Token}&IdGroupe={$IdGroupe}&Langue={$Langue}&VisitorLanguage={$VisitorLanguage}";
+		return json_decode(file_get_contents($url));
+	}
 }
